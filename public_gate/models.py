50a901381eac704212dd0f28558515dde9c36625
betezed <bentz.romain@gmail.com>
2015-03-16 20:21:41 +0100

    Add Restrictions class and EmailAccount class

--- public_gate/models.py
+++ public_gate/models.py
@@ -1,3 +1,129 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class BaseProperty(models.Model):
+    """
+    Describes base attributes for any property
+    """
+    payload_display_name = models.CharField(max_length=100)
+    payload_description = models.CharField(max_length=100)
+    payload_identifier = models.CharField(max_length=100, unique=True)
+    payload_organization = models.CharField(max_length=100)
+    payload_version = models.CharField(max_length=100)
+    payload_uuid = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        abstract = True
+
+
+class PropertyList(BaseProperty):
+    """
+    Describes primary property
+    """
+    payload_type = models.CharField(auto_created="Configuration")
+    removal_disallowed = models.CharField(max_length=15, choices=(('never', "Never"),
+                                                                  ('authorization', "With authorization"),
+                                                                  ('always', "Always")))
+
+    def __str__(self):
+        return self.display_name + " - " + self.description
+
+    def save(self, *args, **kwargs):
+        super(PropertyList, self).save(*args, **kwargs)
+
+
+class EmailAccount(BaseProperty):
+    """
+    Describes all necessary properties for mail configuration
+    """
+    property_list = models.OneToOneField(PropertyList)
+    payload_type = models.CharField(auto_created="com.apple.mail.managed")
+    email_account_description = models.CharField(max_length=100)
+    email_account_name = models.CharField(max_length=100)
+    email_account_type = models.CharField(max_length=15, choices=(
+        ("EmailTypePOP", "POP"),
+        ("EmailTypeIMAP", "IMAP")))
+    email_address = models.CharField(max_length=100)
+    incoming_mail_server_authentication = models.CharField(max_length=20, choices=(
+        ("EmailAuthNone", "None"),
+        ("EmailAuthPassword", "Password"),
+        ("EmailAuthCRAMMD5", "MD5 challenge"),
+        ("EmailAuthNTLM", "NTLM"),
+        ("EmailAuthHTTPMD5", "Condensed MD5 HTTP")))
+    incoming_mail_server_host_name = models.CharField(max_length=100)
+    incoming_mail_server_IMAP_prefix = models.CharField(max_length=100)
+    incoming_mail_server_port_number = models.IntegerField(default=143)
+    incoming_mail_server_use_SSL = models.BooleanField(default=False)
+    incoming_mail_server_user_name = models.CharField(max_length=100)
+    incoming_password = models.CharField(max_length=100)
+    outgoing_mail_server_authentication = models.CharField(max_length=20, choices=(
+        ("EmailAuthNone", "None"),
+        ("EmailAuthPassword", "Password"),
+        ("EmailAuthCRAMMD5", "MD5 challenge"),
+        ("EmailAuthNTLM", "NTLM"),
+        ("EmailAuthHTTPMD5", "Condensed MD5 HTTP")))
+    outgoing_mail_server_host_name = models.CharField(max_length=100)
+    outgoing_mail_server_port_number = models.IntegerField(default=587)
+    outgoing_mail_server_user_SSL = models.BooleanField(default=False)
+    outgoing_mail_server_user_name = models.CharField(max_length=100)
+    outgoing_password = models.CharField(max_length=100)
+    outgoing_password_same_as_incoming_password = models.CharField(max_length=100)
+    prevent_app_sheet = models.BooleanField(default=False)
+    prevent_move = models.BooleanField(default=False)
+    SMIME_enabled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email_account_name + " - " + self.email_account_description
+
+    def save(self, *args, **kwargs):
+        super(EmailAccount, self).save(*args, **kwargs)
+        
+        
+class Restrictions(BaseProperty):
+    """
+    Describes all restrictions properties
+    """
+    payload_type = models.CharField(auto_created="com.apple.applicationaccess")
+    allow_adding_game_center_friends = models.BooleanField(default=True)
+    allow_app_installation = models.BooleanField(default=True)
+    allow_assistant = models.BooleanField(default=True)
+    allow_assistant_while_locked = models.BooleanField(default=True)
+    allow_bookstore_erotica = models.BooleanField(default=True)
+    allow_camera = models.BooleanField(default=True)
+    allow_cloud_backup = models.BooleanField(default=True)
+    allow_cloud_document_sync = models.BooleanField(default=True)
+    allow_diagnostic_submission = models.BooleanField(default=True)
+    allow_explicit_content = models.BooleanField(default=True)
+    allow_global_background_fetch_when_roaming = models.BooleanField(default=True)
+    allow_in_app_purchases = models.BooleanField(default=True)
+    allow_multiplayer_gaming = models.BooleanField(default=True)
+    allow_passbook_while_locked = models.BooleanField(default=True)
+    allow_photo_stream = models.BooleanField(default=True)
+    allow_safari = models.BooleanField(default=True)
+    allow_screen_shot = models.BooleanField(default=True)
+    allow_shared_stream = models.BooleanField(default=True)
+    allow_untrusted_tLSPrompt = models.BooleanField(default=True)
+    allow_video_conferencing = models.BooleanField(default=True)
+    allow_voice_dialing = models.BooleanField(default=True)
+    allow_you_tube = models.BooleanField(default=True)
+    allow_iTtunes = models.BooleanField(default=True)
+    force_encrypted_backup = models.BooleanField(default=True)
+    force_iTunes_store_password_entry = models.BooleanField(default=True)
+    rating_apps = models.BooleanField(default=True)
+    rating_movies = models.BooleanField(default=True)
+    rating_region = models.BooleanField(default=True)
+    rating_tVShows = models.BooleanField(default=True)
+    safari_accept_cookies = models.BooleanField(default=True)
+    safari_allow_auto_fill = models.BooleanField(default=True)
+    safari_allow_java_script = models.BooleanField(default=True)
+    safari_allow_popups = models.BooleanField(default=True)
+    safari_force_fraud_warning = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.email_account_name + " - " + self.email_account_description
+
+    def save(self, *args, **kwargs):
+        super(Restrictions, self).save(*args, **kwargs)
