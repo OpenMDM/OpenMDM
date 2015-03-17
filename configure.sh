0175d2fbdb08658d10fd0c382085e1fa628e9f60
betezed <bentz.romain@gmail.com>
2015-03-17 23:11:06 +0100

    Better checking of python interpreter

--- configure.sh
+++ configure.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-printf "Python 3.4 binary [python3.4] "
-read -r PYTHON_BINARY
-if [ "$PYTHON_BINARY" == "" ]
+PYTHON3_DEFAULT_PATH=$(which python3)
+echo -ne "Please enter the path to your Python 3.4 [default ${PYTHON3_DEFAULT_PATH}] "
+read PYTHON3_PATH
+if [ -z ${PYTHON3_PATH} ]
 then
-    PYTHON_BINARY="python3.4"
+    PYTHON3_PATH=${PYTHON3_DEFAULT_PATH}
 fi
-SITE_PACKAGE=$(python3.4 -c 'import site; print(site.getsitepackages()[0])')
-mv ./mysql $SITE_PACKAGE
+CHECK_VERSION=$(${PYTHON3_PATH} -c 'import sys; print(sys.version_info[:2])' 2>/dev/null)
+if [ "${CHECK_VERSION}" != "(3, 4)" ]
+then
+    echo  "Invalid Python interpreter, version 3.4 required"
+    exit -1
+fi
+SITE_PACKAGE=$(${PYTHON3_PATH} -c 'import site; print(site.getsitepackages()[0])')
+mv ./mysql ${SITE_PACKAGE} 2>/dev/null
 echo "[OK] MySQL connector added"
 echo "Creating super user"
-$PYTHON_BINARY manage.py createsuperuser
-$PYTHON_BINARY manage.py makemigrations public_gate
-$PYTHON_BINARY manage.py migrate
+${PYTHON3_PATH} manage.py createsuperuser
+${PYTHON3_PATH} manage.py makemigrations public_gate
+${PYTHON3_PATH} manage.py migrate
