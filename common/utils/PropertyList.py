01869f416f67e2ad610dbfdc657c854eb4bfbd99
betezed <bentz.romain@gmail.com>
2015-03-18 17:21:12 +0100

    Propper format of variable from underscored to camel cased + capitalized

--- common/utils/PropertyList.py
+++ common/utils/PropertyList.py
@@ -9,7 +9,18 @@ def format_object_to_plist(obj):
     """
     dictionary = json.loads(serializers.serialize('json', [obj]))
     dictionary = dictionary[0]['fields']
-    return {(re.sub(r"_([a-z0-9])", underscore_to_camel_case, str(key))).capitalize():  value for key, value in dictionary.items()}
+    result = {}
+    for key, value in dictionary.items():
+        # The goal here is to transform variable looking like this
+        #   this_is_1_random_var
+        # to this
+        #   ThisIs1RandomVar
+        # So next line replaces ever _x with X
+        key = re.sub(r"_([a-z0-9])", underscore_to_camel_case, str(key))
+        # And this lines replaces the first letter with it's upper version
+        key = key[:1].capitalize() + key[1:]
+        result[key] = value
+    return result
 
 
 def underscore_to_camel_case(match):
