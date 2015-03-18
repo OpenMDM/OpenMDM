0ca23568c88447043a7854263b585454539b33f9
betezed <bentz.romain@gmail.com>
2015-03-18 14:42:21 +0100

    add admin bootstrap

--- OpenMDM/settings.py
+++ OpenMDM/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
