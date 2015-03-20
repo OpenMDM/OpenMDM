9b7b338f49ee695c00f68ba68d380893cd98ac54
betezed <bentz.romain@gmail.com>
2015-03-20 09:50:07 +0100

    External config

--- OpenMDM/settings.py
+++ OpenMDM/settings.py
@@ -110,20 +110,17 @@ RETRIEVE_PLIST_FROM_GROUPS = "first"
 
 # LDAP
 
-AUTH_LDAP_SERVER_URI = "ldap://hackndo.com"
+AUTH_LDAP_SERVER_URI = CONFIG['local']['ldap']['SERVER_URI']
 # Diect bind
 # AUTH_LDAP_USER_DN_TEMPLATE = "cn=%(user)s,ou=users,dc=ldap,dc=hackndo,dc=com"
 # Search / Bind
-AUTH_LDAP_BIND_DN = "cn=John Doe,ou=users,dc=ldap,dc=hackndo,dc=com"
-AUTH_LDAP_BIND_PASSWORD = "test"
-AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=users,dc=ldap,dc=hackndo,dc=com",
-    ldap.SCOPE_SUBTREE, "(cn=%(user)s)")
+AUTH_LDAP_BIND_DN = CONFIG['local']['ldap']['BIND_DN']
+AUTH_LDAP_BIND_PASSWORD = CONFIG['local']['ldap']['BIND_PASSWORD']
+AUTH_LDAP_USER_SEARCH = CONFIG['local']['ldap']['USER_SEARCH']
 
 # Finding groups
-AUTH_LDAP_GROUP_SEARCH = LDAPSearch("ou=groups,dc=ldap,dc=hackndo,dc=com",
-    ldap.SCOPE_SUBTREE, "(objectClass=posixGroup)"
-)
-AUTH_LDAP_GROUP_TYPE = PosixGroupType()
-AUTH_LDAP_REQUIRE_GROUP = "cn=finance,ou=groups,dc=ldap,dc=hackndo,dc=com"
+AUTH_LDAP_GROUP_SEARCH = CONFIG['local']['ldap']['GROUP_SEARCH']
+AUTH_LDAP_GROUP_TYPE = CONFIG['local']['ldap']['GROUP_TYPE']
+AUTH_LDAP_REQUIRE_GROUP = CONFIG['local']['ldap']['REQUIRE_GROUP']
 
-connect('mdm')
+connect(CONFIG['local']['mongo']['DB'])
